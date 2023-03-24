876c8813d7bf73d82683df84f820b94a13b2fa26
rayvav2 <rayvav2@illinois.edu>
2023-03-24 17:22:41 -0500

    map placeholder updated

--- pages/subpages/map.tsx
+++ pages/subpages/map.tsx
@@ -1,3 +1,12 @@
+import myImage from '/Users/rayva/Desktop/scfwebsite-3/public/union.jpg';
+import { StaticImageData } from 'next/image';
+
 export function Map() {
-    return <></>
+    const myImageString: string = (myImage as StaticImageData).toString();
+    return (
+        <>
+          <h1>Map</h1>
+          <img src={myImage.src} alt="My Image" />
+        </>
+      );
 }
