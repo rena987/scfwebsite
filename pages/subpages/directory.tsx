477567665dec769af185e6f20eb60d183b8e9d6a
rena987 <47072485+rena987@users.noreply.github.com>
2023-04-13 17:23:03 -0500

    filtering functionality updated

--- pages/subpages/directory.tsx
+++ pages/subpages/directory.tsx
@@ -1,4 +1,6 @@
 import styles from '@/styles/Directory.module.css';
+import OrgCard from './orgCard';
+import Image from 'next/image';
 
 export function Directory() {
     return (<>
@@ -25,14 +27,20 @@ export function Directory() {
             </div>
 
             <div className={styles.rightColumn}>
-                <div className={styles.searchBox}>
-                    4 results
-                </div>
+                <label role="search" className={styles.searchBox}>
+                    <i className={styles.searchIcon} />
+                    <input
+                        type="text"
+                        placeholder="Search for a startup..."
+                    />
+                </label>
                 <div className={styles.resultsNum}>
                     3 results
                 </div>
                 <div className={styles.directory}>
-
+                    <OrgCard />
+                    <OrgCard />
+                    <OrgCard />
                 </div>
             </div>
         </div>
